d906c2f76fe0b5d22088c566423bb0dd11b01cc3
pibragimov <pibragimov@whoosh.bike>
2022-10-23 12:59:03 +0300

    ref: add web reqs for ml

--- ml/app/presentation/routers/router_app.py
+++ ml/app/presentation/routers/router_app.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, UploadFile
 
-from presentation.dependencies import get_service_storage
+from presentation.dependencies import get_service_prediction
 from presentation.logs_utils import LoggerRouteHandler
-from service.service_storage import ServiceStorage
+from service.service_prediction import ServicePrediction
 
 router = APIRouter(prefix="", route_class=LoggerRouteHandler)
 
@@ -10,6 +10,6 @@ router = APIRouter(prefix="", route_class=LoggerRouteHandler)
 @router.post("/predict")
 def upload_weights(
     file: UploadFile,
-    service_storage: ServiceStorage = Depends(get_service_storage),
+    service_prediction: ServicePrediction = Depends(get_service_prediction),
 ):
-    return service_storage.safe_weights_file(file)
+    ...
